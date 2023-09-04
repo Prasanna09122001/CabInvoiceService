4d95a685a912dac8a3981d05b7a396cf6b44a89f
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-09-04 12:09:10 +0530

    [Prasanna] Add:Completon of NS Testing of cab Invoice Service Test

---
+++ CabServiceInvoice/CabServiceInvoice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CabServiceInvoice
+{
+    class program
+    {
+        static void Main()
+        {
+            InvoiceService invoiceSerice = new InvoiceService();
+            string userid = "Prasanna";
+            Ride[] ride =
+            {
+                new Ride(){Distance=10,time=5},
+                new Ride(){Distance=10,time=15},
+            };
+            RideRepository rideRepository = new RideRepository();
+            rideRepository.AddRides(userid, ride);
+            Console.WriteLine(invoiceSerice.CalculateFare(rideRepository.GetRides(userid)));
+        }
+    }
+}
---
+++ CabServiceInvoice/CabServiceInvoice/Ride.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabServiceInvoice
+{
+    public class Ride
+    {
+        public double Distance { get; set; }
+        public int time { get; set; }
+        public string type { get; set; }
+    }
+}
